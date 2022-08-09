d6455ea6f07fd4dc6a186b7069d74fc522914ed3
mohammadmo116 <mohammad1162002@gmail.com>
2022-08-09 19:13:38 +0300

    replication

--- orderR/app/Http/Controllers/PurchaseController.php
+++ orderR/app/Http/Controllers/PurchaseController.php
@@ -21,7 +21,7 @@ class PurchaseController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     *  Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
